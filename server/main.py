79575d92814414d91cd994e99483e7aef9b55e5c
joaovictorbarrera <joao.victor.lotfi@gmail.com>
2022-12-15 14:38:21 -0500

    added secure flag

--- server/main.py
+++ server/main.py
@@ -41,9 +41,10 @@ CORS(app, origins=["http://192.168.0.3:3000"], supports_credentials=True)
 
 # app.config["SESSION_COOKIE_DOMAIN"] = "http://127.0.0.1"
 app.config['SESSION_COOKIE_SAMESITE'] = 'None'
-app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(days=7)
+app.config['SESSION_COOKIE_SECURE'] = True
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["SESSION_FILE_DIR"] = os.path.join(DIR, "sessions")
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(days=3650)
 app.secret_key = str(datetime.now())
 
 Session(app)
